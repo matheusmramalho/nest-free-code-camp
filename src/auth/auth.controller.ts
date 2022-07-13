b4986e8896c060f97b96ed9ce6dd0f6f6bece6fc
Matheus R <matheusramalho@hotmail.com.br>
2022-07-13 20:31:58 -0300

    inclusao de validação com calsse DTO para requisicao do signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    // Com o req do express, sem tipagem DTO para o que vem do request
+    // @Post('signup')
+    // signup(@Req() req: Request) {
+    //     console.log(req.body)
+    //     return this.authService.signup()
+    // }
+
     @Post('signup')
-    signup(@Req() req: Request) {
-        console.log(req.body)
+    signup(@Body() dto: AuthDto) {
+        console.log({ dto: dto })
         return this.authService.signup()
     }
 }
