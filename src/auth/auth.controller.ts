e9a6943b9c568695b6c11b7f0babafd76b53803d
Matheus R <matheusramalho@hotmail.com.br>
2022-07-13 20:01:14 -0300

    configurado banco e feito uma requisicao body para auth signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('signup')
+    signup(@Req() req: Request) {
+        console.log(req.body)
+        return this.authService.signup()
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
-
+import { User, Bookmark } from '@prisma/client'
+import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+
+    constructor(private prismaService: PrismaService) {
+
+    }
+    signup() {
+        // const user: User = {
+        //     firstName: '123'
+        // }
+
+        
+    }
+}
